b72edb21494cd8d40a91d98b77b6df2e17a59ca3
umakant-rails <rajpootumakant@gmail.com>
2024-01-07 09:02:48 +0530

    fix search icon issue

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -50,11 +50,11 @@ const Home = () => {
   return (
     <div>
       <div className='grid grid-cols-5 mb-5'>
-        <form className="col-start-2 col-span-3">   
+        <form className="col-start-2 col-span-3 grow">   
           <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
           <div class="relative">
-            <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-              <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+            <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none" style={{'z-index': '1000'}}>
+              <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="none" viewBox="0 0 20 20">
                 <path stroke="currentColor" strokeLinecap="round" strokeLnejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
               </svg>
             </div>
@@ -73,16 +73,7 @@ const Home = () => {
         नवीनतम रचनायें
       </div>
       <div className='mb-8'>
-        <Swiper
-          slidesPerView={3}
-          spaceBetween={30}
-          freeMode={true}
-          pagination={{
-            clickable: true,
-          }}
-          modules={[FreeMode, Pagination]}
-          className="mySwiper"
-        >
+        <Swiper slidesPerView={3} spaceBetween={30}>
           { getSlideSwipterContent() }
         </Swiper>
       </div>
