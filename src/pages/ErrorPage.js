b11703afcca428517ceb03b045505b90bf93de6c
umakant-rails <rajpootumakant@gmail.com>
2024-01-14 21:00:00 +0530

    update error page html

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -3,18 +3,60 @@ import { useRouteError, Link } from "react-router-dom";
 
 const ErrorPage = () => {
   const error = useRouteError();
-  console.error(error);
-
+  
   return (
-    <section className="bg-white dark:bg-gray-900 mt-28 mb-8">
+    <section className="bg-white dark:bg-gray-900 mt-10 mb-20">
       <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
         <div className="mx-auto max-w-screen-sm text-center">
           <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">404</h1>
           <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">Something's missing.</p>
           <p className="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">Sorry, we can't find that page. You'll find lots to explore on the home page. </p>
-          <Link to="/" className="inline-flex text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4">
-            Back to Homepage
-          </Link>
+        </div>
+      </div>
+      <div className='grid grid-cols-12'>
+        <div className='col-start-3 col-span-8'>
+          <div className='grid grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
+            <div className='relative py-8 px-20 md:px-8 shadow-2xl justify-center items-center border border-blue-500 rounded-md'>
+              <Link to="/" className='flex justify-center items-center'>
+                <svg className="w-[31px] h-[31px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="0.7" d="M3 8v10a1 1 0 0 0 1 1h4v-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v5h4a1 1 0 0 0 1-1V8M1 10l9-9 9 9"/>
+                </svg>
+              </Link>
+              <Link to="/" className="flex justify-center items-center text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4">
+                Home
+              </Link>
+            </div>
+            <div className='relative py-8 px-20 md:px-8 shadow-2xl justify-center items-center border border-blue-500 rounded-md'>
+              <Link to="/" className='flex justify-center items-center'>
+              <svg className="w-[31px] h-[31px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="0.7" d="M10 19a9 9 0 1 0 0-18 9 9 0 0 0 0 18Zm0 0a8.949 8.949 0 0 0 4.951-1.488A3.987 3.987 0 0 0 11 14H9a3.987 3.987 0 0 0-3.951 3.512A8.948 8.948 0 0 0 10 19Zm3-11a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
+              </svg>
+              </Link>
+              <Link to="/" className="flex justify-center items-center text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4">
+                रचनाकार
+              </Link>
+            </div>
+            <div className='relative py-8 px-20 md:px-8 shadow-2xl justify-center items-center border border-blue-500 rounded-md'>
+              <Link to="/" className='flex justify-center items-center'>
+              <svg className="w-[31px] h-[31px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 18">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="0.7" d="M10 16.5c0-1-8-2.7-9-2V1.8c1-1 9 .707 9 1.706M10 16.5V3.506M10 16.5c0-1 8-2.7 9-2V1.8c-1-1-9 .707-9 1.706"/>
+              </svg>
+              </Link>
+              <Link to="/" className="flex justify-center items-center text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4">
+                रसिक वाणी
+              </Link>
+            </div>
+            <div className='relative py-8 px-20 md:px-8 shadow-2xl justify-center items-center border border-blue-500 rounded-md'>
+              <Link to="/" className='flex justify-center items-center'>
+                <svg className="w-[31px] h-[31px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 20">
+                  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="0.7" d="M4.828 10h6.239m-6.239 4h6.239M6 1v4a1 1 0 0 1-1 1H1m14-4v16a.97.97 0 0 1-.933 1H1.933A.97.97 0 0 1 1 18V5.828a2 2 0 0 1 .586-1.414l2.828-2.828A2 2 0 0 1 5.828 1h8.239A.97.97 0 0 1 15 2Z"/>
+                </svg>
+              </Link>
+              <Link to="/" className="flex justify-center items-center text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4">
+                आरती/स्तोत्र
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </section>
