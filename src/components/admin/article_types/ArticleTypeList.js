072a754b93749663c969df641fdfc75477616313
umakant-rails <rajpootumakant@gmail.com>
2024-02-19 18:01:57 +0530

    update article type form name in html

--- src/components/admin/article_types/ArticleTypeList.js
+++ src/components/admin/article_types/ArticleTypeList.js
@@ -147,7 +147,7 @@ const ArticleTypeList = () => {
                      <tr key={index} 
                         className="border-b dark:border-gray-700 text-blue-500 cursor-pointer" >
                         <td className='px-2 py-3'>{(currentPage-1)*10 + (index+1)}</td>
-                        <td scope="row" 
+                        <td 
                           className="px-2 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                             {context.name}
                         </td>
@@ -197,7 +197,7 @@ const ArticleTypeList = () => {
           <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
             <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
               <h3 className="text-xl font-semibold text-gray-900 dark:text-white text-center">
-                { editableType ? 'प्रसंग अद्यतन फॉर्म' : 'प्रसंग फॉर्म'}
+                { editableType ? 'रचना प्रकार अद्यतन फॉर्म' : 'रचना प्रकार फॉर्म'}
               </h3>
               <button type="button" 
                 onClick={hidePopup}
@@ -256,11 +256,11 @@ const ArticleTypeList = () => {
                 editableType ? ( <button type="button"
                   onClick={updateToType} 
                   className="mr-5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                  प्रसंग अद्यतन करें
+                  रचना प्रकार अद्यतन करें
                 </button>) : ( <button type="button"
                   onClick={createNewType} 
                   className="mr-5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                  प्रसंग जोड़े
+                  रचना प्रकार जोड़े
                 </button> ) 
               }
               <button 
